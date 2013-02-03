7f078fa30999c39b91f3ac2279f24f4f18d0462a
Hyun Woo Jung <hw3jung@gmail.com>
2013-02-03 14:05:27 -0500

    HomeController - Kevin
    Testing merge

--- BRApplication/BRApplication/Controllers/HomeController.cs
+++ BRApplication/BRApplication/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace BRApplication.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application. This is Kevin.";
 
             return View();
         }
