ece0e29910d8c9150c7b8c3130e4c152b6082aa1
Hyun Woo Jung <hw3jung@gmail.com>
2013-02-16 23:34:32 -0500

    Testing DB call

--- BRApplication/BRApplication/Controllers/HomeController.cs
+++ BRApplication/BRApplication/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.SqlClient;
+using System.Configuration; 
 
 namespace BRApplication.Controllers
 {
@@ -10,15 +12,34 @@ namespace BRApplication.Controllers
     {
         public ActionResult Index()
         {
+           
+
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application. This is Kevin.";
 
             return View();
         }
 
+        public JsonResult InsertIt(int x)
+        {
+            string connString = System.Web.Configuration.WebConfigurationManager.ConnectionStrings["DB1"].ToString();
+            using (SqlConnection connection = new SqlConnection(connString))
+            using (SqlCommand command = connection.CreateCommand())
+            {
+                command.CommandText = "INSERT INTO TestTable (Test) VALUES (@param)";
+
+                command.Parameters.AddWithValue("@param", x);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+
+            return Json(string.Format("Successfully Inserted:{0}", x));
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "ASMA";
-
+            
             return View();
         }
 
