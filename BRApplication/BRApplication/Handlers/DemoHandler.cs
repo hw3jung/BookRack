77e3465bd5f400e5bf30abb72513ebcecc67f754
MyName <asma.patel@hotmail.com>
2013-02-17 23:45:27 -0500

    Demo Controller, Handler, Model
    Example of an Ajax call back to an MVC controller & one way to make use
    of MVC Partial Views

---
+++ BRApplication/BRApplication/Handlers/DemoHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using BRApplication.Models; 
+
+namespace BRApplication.Handlers
+{
+    public class DemoHandler
+    {
+        public bool InsertStatus(DemoModel Status)
+        {
+            bool Success = true;
+            try
+            {
+                string connString = System.Web.Configuration.WebConfigurationManager.ConnectionStrings["BookRack"].ToString();
+                using (SqlConnection connection = new SqlConnection(connString))
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "INSERT INTO TransactionStatus VALUES (@param)";
+                    command.Parameters.AddWithValue("@param", Status.StatusName);
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception)
+            {
+                Success = false;
+            }
+
+            return Success; 
+        }
+    }
+}
