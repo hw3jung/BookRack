49760482d0b02921aba94e0e0ad4ddca17bc7e23
Hyun Woo Jung <hw3jung@gmail.com>
2013-02-20 21:13:31 -0500

    MarketController
    New controller for the Posts page

---
+++ BRApplication/BRApplication/Controllers/MarketController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BRApplication.Controllers
+{
+    public class MarketController : Controller
+    {
+        //
+        // GET: /Market/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
