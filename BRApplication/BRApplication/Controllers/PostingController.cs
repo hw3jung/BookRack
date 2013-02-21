8154fe4189390b38a4f1f5a8e63d0bd911e6a7dd
Johnny Chen <johnny.uw@gmail.com>
2013-02-21 14:24:51 -0800

    Existing Postings controller and view

---
+++ BRApplication/BRApplication/Controllers/PostingController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BRApplication.Controllers
+{
+    public class PostingController : Controller
+    {
+        //
+        // GET: /Posting/
+
+        public ActionResult Index()
+        {
+            ViewBag.Course = "AFM 102";
+            ViewBag.Condition = "Good";
+            ViewBag.PostedBy = "John Smith";
+            ViewBag.Date = new DateTime(2013, 2, 21, 12, 0, 0);
+            ViewBag.ISBN = "10 0-470-56479-2";
+            ViewBag.price = 50.00;
+            ViewBag.Author = "Walter T. Harrison";
+            return View();
+        }
+
+    }
+}
