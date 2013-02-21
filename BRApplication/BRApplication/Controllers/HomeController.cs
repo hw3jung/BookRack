63842f18b5707a6b5da94bb4c50dfec16b4cecd8
MyName <asma.patel@hotmail.com>
2013-02-20 19:36:13 -0500

    resolving conflict attempt #3

--- BRApplication/BRApplication/Controllers/HomeController.cs
+++ BRApplication/BRApplication/Controllers/HomeController.cs
@@ -12,8 +12,6 @@ namespace BRApplication.Controllers
     {
         public ActionResult Index()
         {
-           
-
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application. This is Kevin.";
 
             return View();
