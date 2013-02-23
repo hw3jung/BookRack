0eec77cc7eee4db371848f89876ebd444c967f4d
MyName <asma.patel@hotmail.com>
2013-02-23 15:02:45 -0500

    Beginning Market Page
    - Added a model representing a market post
    - created a partial page for a sell post (market version)
    - created a partial page for a buy post (market version)

--- BRApplication/BRApplication/Controllers/MarketController.cs
+++ BRApplication/BRApplication/Controllers/MarketController.cs
@@ -14,7 +14,18 @@ namespace BRApplication.Controllers
 
         public ActionResult Index()
         {
-            return View();
+            List<MarketPostModel> mpmLst = new List<MarketPostModel>();
+            MarketPostModel mpm1 = new MarketPostModel(false, "AFM 101", "Excellent", "Asma Patel", DateTime.Now, "978-3-16-148410-0", "Mark Twain", 300);
+            MarketPostModel mpm2 = new MarketPostModel(false, "MATH 135", "Good", "Johnny Depp", DateTime.Now, "978-3-16-148410-0", "John Jacob JingleHeimer", 150.99);
+            MarketPostModel mpm3 = new MarketPostModel(false, "STAT 230", "Good", "Jack Johnson", DateTime.Now, "978-3-16-148410-0", "Kiets d a", 0.99);
+            MarketPostModel mpm4 = new MarketPostModel(false, "ACTS 230", "Good", "Hugh Grant", DateTime.Now, "978-3-16-148410-0", "Homer J. Simpson", 500.99);
+            
+            mpmLst.Add(mpm1);
+            mpmLst.Add(mpm2);
+            mpmLst.Add(mpm3);
+            mpmLst.Add(mpm4);
+
+            return View(mpmLst); 
         }
 
     }
---
+++ BRApplication/BRApplication/Models/MarketPostModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BRApplication.Models
+{
+    public class MarketPostModel
+    {
+        public bool IsBuy { get; set; }
+        public string Course { get; set; }
+        public string Condition { get; set; }
+        public string PostedBy { get; set; }
+        public DateTime datePosted { get; set; }
+        public string? ISBN { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+
+        public MarketPostModel(bool isBuy, string course, string condition, string postedby, DateTime dateposted, string? isbn, string author, decimal price)
+        {
+            this.IsBuy = isBuy;
+            this.Course = course;
+            this.Condition = condition;
+            this.PostedBy = postedby;
+            this.datePosted = dateposted;
+            this.ISBN = isbn;
+            this.Author = author;
+            this.Price = price; 
+        }
+    }
+}
