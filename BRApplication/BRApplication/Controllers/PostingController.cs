1bfbedc66112608cbf0223a94a00828c63a50542
MyName <asma.patel@hotmail.com>
2013-02-23 18:46:55 -0500

    Posting Controller
    Pass in a model so that the page isn't broken when we navigate to it.
    
    Cheerio!
    Asma, the Great

--- BRApplication/BRApplication/Controllers/PostingController.cs
+++ BRApplication/BRApplication/Controllers/PostingController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BRApplication.Models; 
 
 namespace BRApplication.Controllers
 {
@@ -13,15 +14,17 @@ namespace BRApplication.Controllers
 
         public ActionResult Index()
         {
-            ViewBag.Title = "Managerial Accounting - Ninth Canadian Edition";
-            ViewBag.Course = "AFM 102";
-            ViewBag.Condition = "Good";
-            ViewBag.PostedBy = "John Smith";
-            ViewBag.Date = new DateTime(2013, 2, 21, 12, 0, 0);
-            ViewBag.ISBN = "10 0-470-56479-2";
-            ViewBag.price = 50.00;
-            ViewBag.Author = "Walter T. Harrison";
-            return View();
+         
+            string Title = "Managerial Accounting - Ninth Canadian Edition";
+            string Course = "AFM 102";
+            string Condition = "Good";
+            string PostedBy = "John Smith";
+            DateTime Date = new DateTime(2013, 2, 21, 12, 0, 0);
+            string ISBN = "10 0-470-56479-2";
+            double price = 50.00;
+            string Author = "Walter T. Harrison";
+            MarketPostModel mpm = new MarketPostModel(Title, false, Course, Condition, PostedBy, Date, ISBN, Author, price); 
+            return View(mpm);
         }
 
         public ActionResult AddPosting()
